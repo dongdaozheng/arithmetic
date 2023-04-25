de421a7a9e8888b0e7f36d9584248bc2e3501092
ddz2456820347 <2456820347@qq.com>
2023-04-25 12:05:14 +0800

    updated

--- arithmetic/02.js
+++ arithmetic/02.js
@@ -10,9 +10,7 @@ var twoSum = function (nums, target) {
 	for (let index = 0; index < nums.length; index++) {
 
 		arr[0] = index
-
 		let number = target - nums[index]
-
 		for (let i = index + 1, j = nums.length - 1; i <= j; i++, j--) {
 			if (nums[i] === number) {
 				arr[1] = i
